84f9948765c012f59a8db24f007f86449b77919c
supersamuj533 <62748014+supersamuj533@users.noreply.github.com>
2021-05-27 21:39:32 +0200

    14

--- app/Http/Controllers/ricercaUt.php
+++ app/Http/Controllers/ricercaUt.php
@@ -10,10 +10,23 @@ use App\Models\twitte;
 
 class ricercaUt extends Controller
 { 
+
+    public function r(Request $request)
+    {
+        $message='';
+        return view('regDim',['message'=> $message]);
+    }
+
+
     public function ricerca(Request $request)
     {  try{
         $dati = $request->input();
-        $parolaChiave=$dati['ricerca'];
+        if(empty($dati['ricerca'])){
+            $message='x        Hai inserito dei dati inesistenti';
+            return view('regDim',['message'=> $message]);
+        }
+        else{
+        $parolaChiave=$dati['ricerca'];}
      $email = DB::table('utente')->where('email','LIKE', $parolaChiave )
                       ->get();
     $numero = DB::table('utente')->where('numero','=', $parolaChiave )
@@ -21,12 +34,26 @@ class ricercaUt extends Controller
     $nomeUtente = DB::table('utente')->where('nomeUtente','LIKE', $parolaChiave )
                       ->get();
                       foreach( $email as  $emails){
-   if($emails->email==$parolaChiave){$utente=$email;}}
+   if($emails->email==$parolaChiave){$utente=$email;}
+
+}
    foreach($nomeUtente as  $nomeUtentes){
-   if($nomeUtentes->nomeUtente==$parolaChiave){$utente=$nomeUtente;}}
+   if($nomeUtentes->nomeUtente==$parolaChiave){$utente=$nomeUtente;}
+
+
+}
    foreach($numero  as  $numeros){
-    if($numeros->numero==$parolaChiave){$utente=$numero;}}
-        return view('regDim2',['utente'=> $utente]);
+    if($numeros->numero==$parolaChiave){$utente=$numero;}
+ 
+
+}
+    $message='';
+    if(empty($utente)){
+        $message='X        Hai inserito dei dati inesistenti';
+        return view('regDim',['message'=> $message]);
+    }
+    else{
+        return view('regDim2',['utente'=> $utente,'message'=> $message]);}
    }
    catch (Exception $e){ return redirect('/cerca2');} 
     
@@ -35,4 +62,3 @@ class ricercaUt extends Controller
 
      
     }
-
